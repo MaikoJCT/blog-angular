4a6afa7283a399df413b1926b77441e9d641cdce
MaikoJCT <maikotoporoski@gmail.com>
2023-04-24 19:56:58 -0300

    Project finished

---
+++ src/app/data/datafake.ts
@@ -0,0 +1,17 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'Novo homem de ferro anunciado.',
+    description:
+      'Marvel studios acaba de anunciar um novo filme do homem de ferro.',
+    photoCover:
+      'https://imgs.search.brave.com/eZiM3BBs15MHgsmvUcyWq2PPJCtvRKD0b-hxeXpc9F8/rs:fit:1200:1050:1/g:ce/aHR0cDovL2ltYWdl/czUuZmFucG9wLmNv/bS9pbWFnZS9waG90/b3MvMzE3MDAwMDAv/SXJvbi1NYW4taXJv/bi1tYW4tMy0zMTc4/MDE4MC0xNjgwLTEw/NTAuanBn',
+  },
+  {
+    id: '2',
+    title: 'Mais um filme dos Vingadores',
+    description: 'bla bla bla',
+    photoCover:
+      'https://imgs.search.brave.com/eB85uuY7hv3tNSOAsr_2E1ryAUe3wlUhpakw1ReFQYM/rs:fit:1200:1080:1/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzc0L2M4/L2VjLzc0YzhlYzFh/ZGFkNjkyNzcyZGE3/OTM0Mzk4NDA0ZWQx/LmpwZw',
+  },
+];
